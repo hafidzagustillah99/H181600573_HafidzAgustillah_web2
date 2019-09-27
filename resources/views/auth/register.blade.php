8da22ee7aaa1e1f5a46ae2aa13e5b0610f79b23d
hafidzagustillah99 <hafidzagustillah99@gmail.com>
2019-09-27 09:20:58 -0700

    first commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,6 +38,19 @@
                                 @enderror
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <label for="kontak" class="col-md-4 col-form-label text-md-right">{{ __('kontak') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="kontak" type="text" class="form-control @error('kontak') is-invalid @enderror" name="kontak" value="{{ old('kontak') }}" required autocomplete="email">
+
+                                @error('kontak')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
